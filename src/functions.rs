154c9921c9b995eef348445b77f2f149dcf16806
j4viersteven <javiersteven503@gmail.com>
2022-08-19 22:16:07 -0500

    move: add_two function to functions file

--- src/functions.rs
+++ src/functions.rs
@@ -1,6 +1,7 @@
 #![allow(unused)]
 
 use rand::Rng;
+use std::io;
 
 pub fn print_range(a: isize, b: isize) -> () {
     for item in a..b+1 {
@@ -10,4 +11,16 @@ pub fn print_range(a: isize, b: isize) -> () {
 
 pub fn num_range(a: isize, b: isize) -> isize {
     rand::thread_rng().gen_range(a..=b)
+}
+
+pub fn add_two() -> i8 {
+    println!("Write two numbers: ");
+    let mut first_number = String::new();
+    let mut second_number = String::new();
+    io::stdin().read_line(&mut first_number);
+    io::stdin().read_line(&mut second_number);
+    let first_number: isize = first_number.trim().parse().expect("Type a number");
+    let second_number: isize = second_number.trim().parse().expect("Type a number");
+    print_range(first_number, second_number);
+    return 0
 }

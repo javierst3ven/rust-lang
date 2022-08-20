ff87806cb7afbe11bae9a3c43c702c29dd27e52c
j4viersteven <javiersteven503@gmail.com>
2022-08-19 22:19:27 -0500

    add: more options in menu function

--- src/menu.rs
+++ src/menu.rs
@@ -1,5 +1,10 @@
 #![allow(unused)]
 
+use std::io;
+
 pub fn menu() -> () {
     println!("MENU");
+    println!("Selecciona una opción: ");
+    let mut number = String::new();
+    io::stdin().read_line(&mut number);
 }
